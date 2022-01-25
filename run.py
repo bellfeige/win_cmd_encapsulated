baeb6ffc4776864dc170bd32f0b35bfcb64e9c2e
bellfeige <ffvch@163.com>
2022-01-25 14:39:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,23 +9,29 @@ ap.add_argument("-u", "--usage_percent", required=False, default=80.0,
                 help="cpu usage percentage")
 ap.add_argument("-d", "--direction", required=False, default='less_than',
                 help="greater_than or less_than")
-ap.add_argument("-t", "--restart-in-second", required=False, default="1",
-                help="shutdown in x seconds")
+ap.add_argument("-a", "--shutdown_argument", required=False, default="/r",
+                help="shutdown argument, default is /r /s /l")
+ap.add_argument("-t", "--execute_in_second", required=False, default="1",
+                help="shutdown /t value")
 args = vars(ap.parse_args())
 
 # print("The CPU usage is : ", cpu_usage)
 cpu_usage = psutil.cpu_percent(4)
-print('The CPU usage is: ', cpu_usage)
 
 if args['direction'] == 'greater_than':
     if cpu_usage > float(args['usage_percent']):
         # 80.0 by default
-        print(f"restart in {args['restart-in-second']} seconds")
-        os.system(f"restart /r /t {args['restart-in-second']}")
-
+        print(f"The CPU usage is: {cpu_usage}")
+        print(f"Executing {args['shutdown_argument']} in {args['execute_in_second']} seconds")
+        os.system(f"shutdown {args['shutdown_argument']} /t {args['execute_in_second']}")
+    else:
+        print(f"The CPU usage is: {cpu_usage} < {args['usage_percent']}. Exiting....")
 # default condition
-if args['direction'] == 'less_than':
+elif args['direction'] == 'less_than':
     if cpu_usage <= float(args['usage_percent']):
         # 80.0 by default
-        print(f"restart in {args['restart-in-second']} seconds")
-        os.system(f"restart in {args['restart-in-second']} seconds")
+        print(f"The CPU usage is: {cpu_usage}")
+        print(f"Executing {args['shutdown_argument']} in {args['execute_in_second']} seconds")
+        os.system(f"shutdown {args['shutdown_argument']} /t {args['execute_in_second']}")
+    else:
+        print(f"The CPU usage is: {cpu_usage} > {args['usage_percent']}. Exiting....")
